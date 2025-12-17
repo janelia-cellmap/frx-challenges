a8e0c3360c0b6e88b13e398389e56747824bc5d1
Jody Clements <jody@neomorphic.co.uk>
2025-12-17 17:24:18 -0500

    Improves display of versions list in Admin.

--- frx_challenges/web/admin.py
+++ frx_challenges/web/admin.py
@@ -1,5 +1,7 @@
 from allauth.account.decorators import secure_admin_login
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from reversion.admin import VersionAdmin
 
 from .models import Collaborator, ContentFile, Evaluation, Page, Submission, Version
@@ -19,7 +21,22 @@ class ContentFileAdmin(VersionAdmin):
 class EvaluationAdmin(VersionAdmin):
     list_display = ("id", "status", "created_at", "last_updated")
 
+@admin.register(Submission)
+class SubmissionAdmin(VersionAdmin):
+    list_display = ("id", "user_link", "name")
 
-admin.site.register([Version, Submission, Collaborator])
+    def user_link(self, obj):
+        url = reverse("admin:auth_user_change", args=[obj.user.id])
+        return format_html('<a href="{}">{}</a>', url, obj.user.username)
+
+    user_link.short_description = "User"
+
+
+@admin.register(Version)
+class VersionModelAdmin(VersionAdmin):
+    list_display = ("id", "user", "submission__name", "filename", "status")
+
+
+admin.site.register([Collaborator])
 
 admin.site.login = secure_admin_login(admin.site.login)
